59bf5239e09106f11b06e84e5175415e9dd2ffc2
reizvoll <mara2277@naver.com>
2025-06-03 20:54:53 +0900

    feat: improve auth structure, seed logic, and add user profile API

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -7,7 +7,6 @@ declare global {
     interface Request {
       user?: {
         id: string;
-        githubId: string;
         username: string;
         image?: string;
       };
@@ -33,7 +32,6 @@ export const authToken = (req: Request, res: Response, next: NextFunction) => {
   try {
     const decoded = jwt.verify(token, authConfig.jwt.secret) as {
       id: string;
-      githubId: string;
       username: string;
       image?: string;
     };
