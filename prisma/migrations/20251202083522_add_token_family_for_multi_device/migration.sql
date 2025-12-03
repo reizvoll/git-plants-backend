152b0bdd9a91efcfd8f787250144ca90d5c387cf
reizvoll <mara2277@naver.com>
2025-12-03 13:43:13 +0900

    chore: revert migration script changes

--- prisma/migrations/20251202083522_add_token_family_for_multi_device/migration.sql
+++ prisma/migrations/20251202083522_add_token_family_for_multi_device/migration.sql
@@ -4,9 +4,6 @@
   - Added the required column `familyId` to the `RefreshToken` table without a default value. This is not possible if the table is not empty.
 
 */
--- Delete existing refresh tokens before adding NOT NULL column
-DELETE FROM "public"."RefreshToken";
-
 -- AlterTable
 ALTER TABLE "public"."RefreshToken" ADD COLUMN     "familyId" TEXT NOT NULL;
 
