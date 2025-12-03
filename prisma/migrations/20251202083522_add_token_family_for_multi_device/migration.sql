8bbbbe2a922e7fcf94330148267c6c63e366f155
reizvoll <mara2277@naver.com>
2025-12-03 13:23:02 +0900

    chore: delete existing refresh tokens before adding NOT NULL familyId column

--- prisma/migrations/20251202083522_add_token_family_for_multi_device/migration.sql
+++ prisma/migrations/20251202083522_add_token_family_for_multi_device/migration.sql
@@ -4,6 +4,9 @@
   - Added the required column `familyId` to the `RefreshToken` table without a default value. This is not possible if the table is not empty.
 
 */
+-- Delete existing refresh tokens before adding NOT NULL column
+DELETE FROM "public"."RefreshToken";
+
 -- AlterTable
 ALTER TABLE "public"."RefreshToken" ADD COLUMN     "familyId" TEXT NOT NULL;
 
